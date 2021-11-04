26e1bf3ceff53e1e8af435e1ede626545af08bd4
dominik24c <dominik24c@gmail.com>
2021-11-04 19:58:28 +0100

    add table league team_league

--- database.sql
+++ database.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS football;
 CREATE DATABASE football;
 
+\c football
+
 DROP TABLE IF EXISTS matches;
 DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS team;
@@ -14,23 +16,21 @@ CREATE TABLE position
 
 CREATE TABLE team
 (
-    id      INT GENERATED ALWAYS AS IDENTITY,
+    id      SERIAL PRIMARY KEY,
     name    varchar(150) NOT NULL,
     stadium varchar(100) NOT NULL,
-    coach   varchar(200) NOT NULL,
-    PRIMARY KEY (id)
+    coach   varchar(200) NOT NULL
 );
 
 CREATE TABLE player
 (
-    id            INT GENERATED ALWAYS AS IDENTITY,
+    id            SERIAL PRIMARY KEY,
     position      varchar(50)  NOT NULL,
     team_id       INT,
     first_name    varchar(255) NOT NULL,
     last_name     varchar(255) NOT NULL,
     country       varchar(255) NOT NULL,
     date_of_birth DATE,
-    PRIMARY KEY (id),
     CONSTRAINT fk_club
         FOREIGN KEY (team_id)
             REFERENCES team (id),
@@ -41,20 +41,47 @@ CREATE TABLE player
 
 CREATE TABLE matches
 (
-    id                       INT GENERATED ALWAYS AS IDENTITY,
+    id                       SERIAL PRIMARY KEY,
+    league_id                INT,
     hosts_team_id            INT NOT NULL,
     guests_team_id           INT NOT NULL,
     goals_of_the_hosts_team  INT       DEFAULT 0,
     goals_of_the_guests_team INT       DEFAULT 0,
     date                     TIMESTAMP DEFAULT NOW(),
-    PRIMARY KEY (id),
     CONSTRAINT fk_hosts_team
         FOREIGN KEY (hosts_team_id)
             REFERENCES team (id),
     CONSTRAINT fk_guests_team
-        FOREIGN KEY (hosts_team_id)
-            REFERENCES team (id)
+        FOREIGN KEY (guests_team_id)
+            REFERENCES team (id),
+    CONSTRAINT fk_league
+        FOREIGN KEY (league_id)
+            REFERENCES league (id)
 );
+
+CREATE TABLE league
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    description TEXT         NOT NULL
+);
+
+CREATE TABLE team_league
+(
+    league_id    INT NOT NULL,
+    team_id      INT NOT NULL,
+    points       INT DEFAULT 0,
+    goals_lost   INT DEFAULT 0,
+    goals_scored INT DEFAULT 0,
+    CONSTRAINT fk_league
+        FOREIGN KEY (league_id)
+            REFERENCES league (id),
+    CONSTRAINT fk_team
+        FOREIGN KEY (team_id)
+            REFERENCES team (id)
+
+);
+
 
 INSERT INTO position
 VALUES ('Goalkeeper'),
@@ -78,11 +105,23 @@ VALUES ('Goalkeeper', 2, 'Andre', 'ter Stagen', 'germany', '1992-04-30'),
        ('Goalkeeper', 4, 'Kepa', 'Arrizabalaga', 'spain', '1994-10-03'),
        ('Midfielder', 4, 'Mason', 'Mount', 'england', '1999-01-10');
 
-INSERT INTO matches (hosts_team_id, guests_team_id, goals_of_the_hosts_team, goals_of_the_guests_team)
-VALUES (1, 2, 3, 0),
-       (3, 4, 2, 2),
-       (2, 3, 2, 1),
-       (4, 1, 0, 0);
+
+INSERT INTO league(name, description)
+VALUES ('Friendly Cup', 'European friendly club for  italian, spanish and english clubs');
+
+INSERT INTO team_league(league_id, team_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4);
+
+INSERT INTO matches (hosts_team_id, guests_team_id, goals_of_the_hosts_team, goals_of_the_guests_team, league_id)
+VALUES (1, 2, 3, 0, 1),
+       (3, 4, 2, 2, 1),
+       (2, 3, 2, 1, 1),
+       (4, 1, 1, 1, 1),
+       (2, 3, 0, 0, NULL);
+
 
 
 SELECT *
@@ -93,3 +132,7 @@ SELECT *
 FROM player;
 SELECT *
 FROM matches;
+SELECT *
+FROM league;
+SELECT *
+FROM team_league;
---
+++ readme.md
@@ -0,0 +1,4 @@
+### 1 Command to create postgres database
+'''bash
+$    sudo -u postgres psql -f database.sql
+'''
